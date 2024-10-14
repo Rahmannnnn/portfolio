b2bb6f1ed0d7f6519d1ec8bac02cc2cb3efd113d
rahmannnnn <arifingatrahman@gmail.com>
2024-10-14 19:47:12 +0700

    feat: add following cursor

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -5,17 +5,49 @@ import { GSAP } from '@/components/GSAP';
 import Navigation from '@/components/Navigation';
 import { NavigationProvider } from '@/contexts/NavigationContext';
 import { TransitionProvider } from '@/contexts/TransitionContext';
+import { useEffect, useRef } from 'react';
+import styles from './layout.module.scss';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const dotRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    let x = 0,
+      y = 0;
+
+    const updatePosition = (event: MouseEvent) => {
+      x = event.clientX;
+      y = event.clientY;
+
+      if (dotRef.current) {
+        dotRef.current.style.left = `${x}px`;
+        dotRef.current.style.top = `${y}px`;
+        dotRef.current.animate(
+          {
+            left: `${x}px`,
+            top: `${y}px`,
+          },
+          {
+            duration: 500,
+            fill: 'forwards',
+          }
+        );
+      }
+    };
+
+    window.addEventListener('mousemove', updatePosition);
+    return () => window.removeEventListener('mousemove', updatePosition);
+  }, []);
+
   return (
     <html lang="en">
       <TransitionProvider>
         <NavigationProvider>
           <body>
+            <div ref={dotRef} className={`${styles.cursor__dot}`}></div>
             <Navigation />
             {children}
             <Footer />
