d2a646ee4732c604d1c431df1619397d832c4e1d
rahmannnnn <arifingatrahman@gmail.com>
2024-10-26 16:06:09 +0700

    feat: add positions

--- src/constants/POSITIONS.ts
+++ src/constants/POSITIONS.ts
@@ -1,41 +1,51 @@
 export const POSITIONS: { top: number; left: number }[] = [
   {
+    // 1
     top: 16,
     left: 10,
   },
   {
+    // 2
     top: 12,
     left: 50,
   },
   {
+    // 3
     top: 16,
     left: 75,
   },
   {
+    // 4
     top: 32,
     left: 30,
   },
   {
+    // 5
     top: 32,
     left: 70,
   },
   {
+    // 6
     top: 48,
     left: 12.5,
   },
   {
+    // 7
     top: 48,
     left: 80,
   },
   {
+    // 8
     top: 64,
     left: 8,
   },
   {
+    // 9
     top: 64,
     left: 35,
   },
   {
+    // 10
     top: 64,
     left: 60,
   },
